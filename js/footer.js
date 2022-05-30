3d61bd2eb8ecc4746f6c175286a1256ec22debf9
jayyehice <jayyehice@gmail.com>
2022-05-30 13:36:32 +0800

    first js scroll, image scroll fail

--- js/footer.js
+++ js/footer.js
@@ -1,32 +1,32 @@
 "use strict"
 
-let footer = document.getElementsByTagName("footer")[0];
-let footer_li_right = document.getElementById("footer_li_right");
-let footer_li_left = document.getElementById("footer_li_left");
+let footer = document.getElementById("footer");
+let footer_ul = document.getElementById("footer_ul");
+console.log(getComputedStyle(footer_ul).width);
 
-let footer_height = parseInt(getComputedStyle(footer).height) + parseInt(getComputedStyle(footer_li_right).width);
-let footer_li_left_width = parseInt(getComputedStyle(footer_li_left).width);
-let footer_li_right_width = parseInt(getComputedStyle(footer_li_right).width);
-console.log(parseInt(getComputedStyle(footer).height));
-console.log(footer_li_left_width);
-console.log(footer_li_right_width);
-console.log(footer_height);
-// footer.style.height = 1000;
+let footer_height = parseInt(getComputedStyle(footer).height)+parseInt(getComputedStyle(footer_ul).width)/2;
+
+// let footer_li_right = document.getElementById("footer_li_right");
+// let footer_li_left = document.getElementById("footer_li_left");
+
+// let footer_height = parseInt(getComputedStyle(footer).height) + parseInt(getComputedStyle(footer_li_right).width);
+// let footer_li_left_width = parseInt(getComputedStyle(footer_li_left).width);
+// let footer_li_right_width = parseInt(getComputedStyle(footer_li_right).width);
+
+
 footer.setAttribute("style", `height: ${footer_height}px;`);
-// style.height = 1000;
 
-//console.log(footer.offsetTop);
 
 
 window.addEventListener("scroll", e => {
-    //console.log(window.scrollY);
+    
     if(window.scrollY >= footer.offsetTop){
         
-        let scroll_move = footer_li_left_width - (window.scrollY-footer.offsetTop);
-        console.log(window.scrollY-footer.offsetTop);
-        footer_li_left.setAttribute("style", `width: ${scroll_move}px;`);
-    }else{
-        footer_li_left.removeAttribute("style");
+        let scroll_move = window.scrollY-footer.offsetTop;
+
+        footer_ul.scrollLeft = scroll_move;
+        
+        // console.log(footer_ul.scrollLeft);
     }
     
 })
