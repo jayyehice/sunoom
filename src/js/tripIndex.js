391aece05092afd54b4ce486e512cbc1de0335ca
john21429 <john21429@gmail.com>
2022-06-15 23:11:16 +0800

    new update

--- src/js/tripIndex.js
+++ src/js/tripIndex.js
@@ -1,1 +1,15 @@
-
+window.addEventListener('load',function(){
+    let seaPage = document.getElementsByClassName('SeaItinerary')[0];
+    let landPage = document.getElementsByClassName('LandItinerary')[0];
+    let packPage = document.getElementsByClassName('packageTour')[0];
+    
+    seaPage.addEventListener('click',function(){
+        window.location.href = './sea.html';
+    })
+    landPage.addEventListener('click',function(){
+        window.location.href = './land_tour.html';
+    })
+    packPage.addEventListener('click',function(){
+        window.location.href = './set.html';
+    })
+})
