8b9f6a503c0d91e03134f5013c781238f8105aeb
john21429 <john21429@gmail.com>
2022-06-22 16:36:54 +0800

    tripChoose updated

--- src/js/orderPage.js
+++ src/js/orderPage.js
@@ -1,145 +1,116 @@
 
-// var app = new Vue({
-//     el: "#app" ,
-//     data: function () {
-//         return {
-//             disabled: [], //禁用的日期
-//             value: new Date(), //值
-//             format: "yyyy-MM-dd", //文本框中顯示的日期格式
-//             clear: false , //是否在文本框右側顯示清除按鈕（X）
-//             placeholder: "日期", //水印
-//             position: "left", //日期面板位置，默認null和left 居左，right則靠右
-//             lang: "" //語言，默認中文，可選值：zh-CN、en
-//         };
-//     },
-
-//     methods: {
-//         //鼠標點擊日曆面板上的日期時的事件，
-//         //傳遞的兩個參數均是同一個值，只是類型不同，隨便用哪個看個人需要
-//         // date 是Date 類型
-//         // dateStr 是String 類型
-//         dayClick: function (date, dateStr) {
-//             console.log(date);
-//            console.log(dateStr);
-//             console.log( this .value);
-//         }
-//     },
-//     components: {
-//         calendar: Calendar
-//     }
-// });
-
-
 
 $(function () {	
+
     let vm = new Vue({
         el:'#app',
         data:{
-            count:1,
+            count:0,
         },
         methods: {
             min(){
                 if(this.count >= 2){
                     this.count-=1
-                    
                 }
-            }
-        }
+            },
+
+        },
         
     })
+    //傳送人數
 
 
-    //日月切換效果
-    $('#sunoomChange').addClass('changing');
-    $('#sunoomChange').click(function(){
+    // //日月切換效果
+    // $('#sunoomChange').addClass('changing');
+    // $('#sunoomChange').click(function(){
         
-        if($('#sunoomChange').hasClass('changing')){
-            $('.orderPage').css({
-                color:"#F5F0DD",
-                backgroundColor:"#0C1244"
-            })
-            $('.chooseAdate').css({
-                color:"#0C1244",
-                backgroundColor:"#F5F0DD",
-                border:'#F5F0DD',
-            })
-            $('.shoppingProcess div').css({
-                border:'1px solid #F5F0DD',
-            })
-            $('.shoppingProcess img').attr('src','./img/orderPage/orderPageArrowWhite.png');
+    //     if($('#sunoomChange').hasClass('changing')){
+    //         $('.orderPage').css({
+    //             color:"#F5F0DD",
+    //             backgroundColor:"#0C1244"
+    //         })
+    //         $('.chooseAdate').css({
+    //             color:"#0C1244",
+    //             backgroundColor:"#F5F0DD",
+    //             border:'#F5F0DD',
+    //         })
+    //         $('.shoppingProcess div').css({
+    //             border:'1px solid #F5F0DD',
+    //         })
+    //         $('.shoppingProcess img').attr('src','./img/orderPage/orderPageArrowWhite.png');
 
-            $('.selectDate li').css({
-                border:'1px solid #F5F0DD',
-            })
-            $('.datePickerBox div').css({
-                border:'1px solid #F5F0DD',
-            })
-            $('#NextPage').css({
-                color:"#F5F0DD",
-                backgroundColor:"#0C1244",
-                border:'1px solid #F5F0DD',
-            })
-            $('#NextPage').hover(function(){
-                $('#NextPage').css({
-                    color:"#0C1244",
-                    backgroundColor:"#F5F0DD",
-                    border:'1px solid #0C1244',
-                })
-            })
-            $('#NextPage').mouseleave(function(){
-                $('#NextPage').css({
-                    color:"#F5F0DD",
-                    backgroundColor:"#0C1244",
-                    border:'1px solid #F5F0DD',
-                })
-            })
+    //         $('.selectDate li').css({
+    //             border:'1px solid #F5F0DD',
+    //         })
+    //         $('.datePickerBox div').css({
+    //             border:'1px solid #F5F0DD',
+    //         })
+    //         $('#NextPage').css({
+    //             color:"#F5F0DD",
+    //             backgroundColor:"#0C1244",
+    //             border:'1px solid #F5F0DD',
+    //         })
+    //         $('#NextPage').hover(function(){
+    //             $('#NextPage').css({
+    //                 color:"#0C1244",
+    //                 backgroundColor:"#F5F0DD",
+    //                 border:'1px solid #0C1244',
+    //             })
+    //         })
+    //         $('#NextPage').mouseleave(function(){
+    //             $('#NextPage').css({
+    //                 color:"#F5F0DD",
+    //                 backgroundColor:"#0C1244",
+    //                 border:'1px solid #F5F0DD',
+    //             })
+    //         })
 
-            $('#sunoomChange').removeClass('changing');
+    //         $('#sunoomChange').removeClass('changing');
 
-        }else{
-            $('.orderPage').css({
-                color:"#0C1244",
-                backgroundColor:"#F5F0DD"
-            })
-            $('.chooseAdate').css({
-                color:"#F5F0DD",
-                backgroundColor:"#0C1244",
-                border:'#0C1244',
-            })
-            $('.shoppingProcess div').css({
-                border:'1px solid #0C1244',
-            })
-            $('.shoppingProcess img').attr('src','./img/orderPage/orderPageArrow.png');
-            $('.selectDate li').css({
-                border:'1px solid #0C1244',
-            })
-            $('.datePickerBox div').css({
-                border:'1px solid #0C1244',
-            })
-            $('#NextPage').css({
-                color:"#0C1244",
-                backgroundColor:"#F5F0DD",
-                border:'1px solid #0C1244',
-            })
-            $('#NextPage').hover(function(){
-                $('#NextPage').css({
-                    color:"#F5F0DD",
-                    backgroundColor:"#0C1244",
-                    border:'1px solid #F5F0DD',
-                })
-            })
-            $('#NextPage').mouseleave(function(){
-                $('#NextPage').css({
-                    color:"#0C1244",
-                    backgroundColor:"#F5F0DD",
-                    border:'1px solid #0C1244',
-                })
-            })
+    //     }else{
+    //         $('.orderPage').css({
+    //             color:"#0C1244",
+    //             backgroundColor:"#F5F0DD"
+    //         })
+    //         $('.chooseAdate').css({
+    //             color:"#F5F0DD",
+    //             backgroundColor:"#0C1244",
+    //             border:'#0C1244',
+    //         })
+    //         $('.shoppingProcess div').css({
+    //             border:'1px solid #0C1244',
+    //         })
+    //         $('.shoppingProcess img').attr('src','./img/orderPage/orderPageArrow.png');
+    //         $('.selectDate li').css({
+    //             border:'1px solid #0C1244',
+    //         })
+    //         $('.datePickerBox div').css({
+    //             border:'1px solid #0C1244',
+    //         })
+    //         $('#NextPage').css({
+    //             color:"#0C1244",
+    //             backgroundColor:"#F5F0DD",
+    //             border:'1px solid #0C1244',
+    //         })
+    //         $('#NextPage').hover(function(){
+    //             $('#NextPage').css({
+    //                 color:"#F5F0DD",
+    //                 backgroundColor:"#0C1244",
+    //                 border:'1px solid #F5F0DD',
+    //             })
+    //         })
+    //         $('#NextPage').mouseleave(function(){
+    //             $('#NextPage').css({
+    //                 color:"#0C1244",
+    //                 backgroundColor:"#F5F0DD",
+    //                 border:'1px solid #0C1244',
+    //             })
+    //         })
             
-            $('#sunoomChange').addClass('changing');
-        }
+    //         $('#sunoomChange').addClass('changing');
+    //     }
 
-    })//日月切換效果結束
+    // })//日月切換效果結束
     
     // $('#dateBooking').daterangepicker({
     //     "showISOWeekNumbers": true,
@@ -156,6 +127,7 @@ $(function () {
 
     //日曆操作區塊
     $('#dateBooking').daterangepicker({
+        "timePicker24Hour": true,
         "showISOWeekNumbers": true,
         "showDropdowns": true,
         "alwaysShowCalendars": true,
@@ -163,14 +135,37 @@ $(function () {
         "drops": "auto",
         "minDate":new Date(),
     });
+
     $('#dateBooking').on('apply.daterangepicker', function(ev, picker) {
     console.log(picker.startDate.format('YYYY-MM-DD'));
     console.log(picker.endDate.format('YYYY-MM-DD'));
+
+    //顯示出日期
     $('#beginDate').html(picker.startDate.format('YYYY-MM-DD'));
     $('#endDate').html(picker.endDate.format('YYYY-MM-DD'));
+    
+    //計算天數
+    let Eday = new Date(picker.endDate).getTime();
+    let Sday = new Date(picker.startDate).getTime();
+    let Ddays = Math.abs(Eday - Sday);
+    let theDays = parseInt(Ddays/(1000*3600*24));
+    //顯示天數
+    $('#manyDays').html(theDays+1);
+
+    sendDays.push(Sday,theDays);
+    
+    
     });
 
+    let sendDays=[];
+    
+    //下一頁
     $('#NextPage').on('click',function(){
+        
+        let pepoleNums = $('#pepoleNums').val();
+        localStorage.setItem('pepoleNums',JSON.stringify(pepoleNums));
+        localStorage.setItem('sendDays',JSON.stringify(sendDays));
+        
         if(window.innerWidth < 576){
             window.location.href='tripChooseMobile.html';
         }else{
@@ -178,10 +173,11 @@ $(function () {
         }
         
     });
-
-    $('#tripChoose').change(function(){
-        // console.log(($('#tripChoose').find("option:selected").text()));
-        $('#bookingDays').text(($('#tripChoose').find("option:selected").text()))
-    })
+    //行程選擇用
+    // $('#tripChoose').change(function(){
+    //     // console.log(($('#tripChoose').find("option:selected").text()));
+    //     $('#bookingDays').text(($('#tripChoose').find("option:selected").text()))
+    // })
     
 })
+
