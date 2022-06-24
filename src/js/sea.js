5bd8dfc0dba5865dfc6e8b59b3db42d7d0ae6353
PinkyYoung <pinkylovetosing@gmail.com>
2022-06-24 12:01:12 +0800

    sea page vue and php change complete

--- src/js/sea.js
+++ src/js/sea.js
@@ -6,23 +6,58 @@ $(document).ready(function(){
         el: '#seaActivity',
         data: {     // 變數放這裡！               
             seaActivity_list: [],
+            activity1:[],
+            activity2:[],
+            activity3:[],
+            activity4:[],
+            activity5:[],
         },
-        methods: {},
+        methods: {
+            testChange(){
+                // console.log(this.activity1[4]);
+
+                if(this.activity1[4] == 1){
+                    this.activity1=this.seaActivity_list[19];
+                    this.activity2=this.seaActivity_list[20];
+                    this.activity3=this.seaActivity_list[21];
+                    this.activity4=this.seaActivity_list[22];
+                    this.activity5=this.seaActivity_list[23];
+                }else{
+                    this.activity1=this.seaActivity_list[12];
+                    this.activity2=this.seaActivity_list[13];
+                    this.activity3=this.seaActivity_list[14];
+                    this.activity4=this.seaActivity_list[15];
+                    this.activity5=this.seaActivity_list[16];
+                }
+            }
+        },
         computed: {},
         watch: {},
         beforeCreate() {
             const url = './php/sea.php';
             fetch(url)
                 .then(response => response.json())
-                .then(text => this.map_list = text);
+                .then(text => {
+                    this.seaActivity_list = text;
+                    this.activity1=this.seaActivity_list[12];
+                    this.activity2=this.seaActivity_list[13];
+                    this.activity3=this.seaActivity_list[14];
+                    this.activity4=this.seaActivity_list[15];
+                    this.activity5=this.seaActivity_list[16];
+                });
         },
         created() {
         },
-        mounted() {   
+        mounted() {
         },
-        updated() {           
-        },
-        
+        updated() {
+                
+        },   
     })
 
 })
+
+// setTimeout(function(){
+//     $('#testButton')[0].click();
+//     console.log('點擊完成')
+// }, 5000)
