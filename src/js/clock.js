50f58990f33debda571b78f634de03e697315e4b
jayyehice <jayyehice@gmail.com>
2022-06-30 15:44:03 +0800

    test

--- src/js/clock.js
+++ src/js/clock.js
@@ -2,104 +2,97 @@
 
 $(document).ready(function(){
     //資料庫渲染到前端
-    new Vue({
-        el: '#seaActivity',
-        data: {     // 變數放這裡！               
-            seaActivity_list: [],
-        },
-        methods: {},
-        computed: {},
-        watch: {},
-        
-        beforeCreate() {
-            const url = './php/sea.php';
-            fetch(url)
-                .then(response => response.json())
-                .then(text => this.map_list = text);
-        },
 
-        created() {},
+    
+    let event_num =1;
+    let bg_num = 1;
+    setInterval((timeFloat),5000);
+    function timeFloat(){
+        let timeBegin = parseInt($(".progress-ring__circle").css("stroke-dashoffset"));
+        if(timeBegin > "-1472"){
+            //轉場黑幕
+            $(".blackCover").animate({"opacity":"1"},{duration:0})
+            $(".blackCover").animate({"opacity":"0.3"},{duration:3000})
 
-        mounted() {  
-            let event_num =1;
-            let bg_num = 1;
-            setInterval((timeFloat),5000);
-            function timeFloat(){
-                let timeBegin = parseInt($(".progress-ring__circle").css("stroke-dashoffset"));
-                if(timeBegin > "-1472"){
-                    //轉場黑幕
-                    $(".blackCover").animate({"opacity":"1"},{duration:0})
-                    $(".blackCover").animate({"opacity":"0.3"},{duration:3000})
+            //中心文字$
+            $(`.event-item${event_num}`).css({"display":"none"})
 
-                    //中心文字$
-                    $(`.event-item${event_num}`).css({"display":"none"})
+            $(`.ocean${event_num}`).animate({"top":"135px"})
+            $(`.chName${event_num}`).animate({"top":"200px"})
+            $(`.enName${event_num}`).animate({"top":"300px"})
+            $(`.discover${event_num}`).animate({"top":"350px"})
 
-                    $(`.ocean${event_num}`).animate({"top":"135px"})
-                    $(`.chName${event_num}`).animate({"top":"200px"})
-                    $(`.enName${event_num}`).animate({"top":"300px"})
-                    $(`.discover${event_num}`).animate({"top":"350px"})
+            // -----------------------------------------------------------------------------------------------------------------------
+            event_num = event_num + 1;
+            // -----------------------------------------------------------------------------------------------------------------------
 
-                    if(event_num == 5){
-                        event_num =1
-                        // $(`.event-item${event_num}`).css({"display":"block"})
-                    }
-                    else{
-                        event_num = event_num + 1;
-                    }
+            $(`.event-item${event_num}`).css({"display":"block"})
 
-                    $(`.event-item${event_num}`).css({"display":"block"})
+            $(`.event-item${event_num}`).animate({"opacity":"0"}).animate({"opacity":"1"},{duration:2000})
+            $(`.ocean${event_num}`).animate({"top":"-65px"},{duration:2000})
+            $(`.chName${event_num}`).animate({"top":"0px"},{duration:2300})
+            $(`.enName${event_num}`).animate({"top":"100px"},{duration:2500})
+            $(`.discover${event_num}`).animate({"top":"150px"},{duration:3000})
 
-                    $(`.event-item${event_num}`).animate({"opacity":"0"}).animate({"opacity":"1"},{duration:2000})
-                    $(`.ocean${event_num}`).animate({"top":"-65px"},{duration:2000})
-                    $(`.chName${event_num}`).animate({"top":"0px"},{duration:2300})
-                    $(`.enName${event_num}`).animate({"top":"100px"},{duration:2500})
-                    $(`.discover${event_num}`).animate({"top":"150px"},{duration:3000})
-
-                    // 流動線圈
-                    $(".progress-ring__circle").css("stroke-dashoffset",(timeBegin-368))
+            // 流動線圈
+            $(".progress-ring__circle").css("stroke-dashoffset",(timeBegin-368))
 
 
-                    //換背景
-                    bg_num = bg_num + 1;
-                    $(".banner").addClass(`bg${bg_num}`)
+            //換背景
+            bg_num = bg_num + 1;
+            $(".banner").addClass(`bg${bg_num}`)
 
-                    console.log(event_num)
-                }
+            console.log(event_num)
+        }
 
-                else{
-                    //轉場黑幕
-                    $(".blackCover").animate({"opacity":"1"},{duration:0})
-                    $(".blackCover").animate({"opacity":"0.3"},{duration:3000})
+        else{
+            //轉場黑幕
+            $(".blackCover").animate({"opacity":"1"},{duration:0})
+            $(".blackCover").animate({"opacity":"0.3"},{duration:3000})
 
-                    // 流動線圈
-                    $(".progress-ring__circle").css("stroke-dashoffset",0)
+            // 流動線圈
+            $(".progress-ring__circle").css("stroke-dashoffset",0)
 
-                    //換背景
-                    bg_num = 1;
-                    $(".banner").removeClass('bg2');
-                    $(".banner").removeClass('bg3');
-                    $(".banner").removeClass('bg4');
-                    $(".banner").removeClass('bg5');
+            //換背景
+            bg_num = 1;
+            $(".banner").removeClass('bg2');
+            $(".banner").removeClass('bg3');
+            $(".banner").removeClass('bg4');
+            $(".banner").removeClass('bg5');
 
-                    //中心文字$
-                    $(`.event-item${event_num}`).css({"display":"none"})
+            //中心文字$
+            // $(`.event-item${event_num}`).css({"display":"none"})
 
 
-                    // $(`.event-item${event_num}`).animate({"opacity":"0"}).animate({"opacity":"1"},{duration:2000})
-                    // $(`.ocean${event_num}`).animate({"top":"-65px"},{duration:2000})
-                    // $(`.chName${event_num}`).animate({"top":"0px"},{duration:2300})
-                    // $(`.enName${event_num}`).animate({"top":"100px"},{duration:2500})
-                    // $(`.discover${event_num}`).animate({"top":"150px"},{duration:3000})
-                    
-                    // event_num =1
-                    // $(`.event-item${event_num}`).css({"display":"block"})
+            // -----------------------------------------------------------------------------------------------------------------------
+            //中心文字$
+            $(`.event-item${event_num}`).css({"display":"none"})
 
-                }
-            } 
-        },
-        
-        updated(){},
-        
-    })
+            $(`.ocean${event_num}`).animate({"top":"135px"})
+            $(`.chName${event_num}`).animate({"top":"200px"})
+            $(`.enName${event_num}`).animate({"top":"300px"})
+            $(`.discover${event_num}`).animate({"top":"350px"})
 
+            event_num =1
+            
+
+            $(`.event-item${event_num}`).css({"display":"block"})
+
+            $(`.event-item${event_num}`).animate({"opacity":"0"}).animate({"opacity":"1"},{duration:2000})
+            $(`.ocean${event_num}`).animate({"top":"-65px"},{duration:2000})
+            $(`.chName${event_num}`).animate({"top":"0px"},{duration:2300})
+            $(`.enName${event_num}`).animate({"top":"100px"},{duration:2500})
+            $(`.discover${event_num}`).animate({"top":"150px"},{duration:3000})
+            // -------------------------------------------------------------------------------------------------------------------------
+            // $(`.event-item${event_num}`).animate({"opacity":"0"}).animate({"opacity":"1"},{duration:2000})
+            // $(`.ocean${event_num}`).animate({"top":"-65px"},{duration:2000})
+            // $(`.chName${event_num}`).animate({"top":"0px"},{duration:2300})
+            // $(`.enName${event_num}`).animate({"top":"100px"},{duration:2500})
+            // $(`.discover${event_num}`).animate({"top":"150px"},{duration:3000})
+            
+            // event_num =1
+            // $(`.event-item${event_num}`).css({"display":"block"})
+            console.log(event_num)
+        }
+    } 
 })
