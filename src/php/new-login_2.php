bc0e73558d58722a0ebfeb1442fb880e683e7919
lu193740 <lu193740@gmail.com>
2022-06-30 11:27:15 +0800

    updata

--- src/php/new-login_2.php
+++ src/php/new-login_2.php
@@ -2,20 +2,19 @@
 <?php
 include("connection.php");
 
-
 //以下兩段不能註解，這樣他才能知道要抓到資料
 $email = $_POST["a"];
 $password = $_POST["b"];
 
 $sql = "SELECT * FROM member WHERE account = ? and password = ?";
 
-   //少了pdo。
+//少了pdo。
 $statement = $pdo->prepare($sql);
 $statement->bindParam(1, $email);
 $statement->bindParam(2, $password);
 $statement->execute();   //這段是必要的，要執行sql語法
-
 $data = $statement->fetchAll();
+
 
 if(count($data) > 0){
     session_start();
