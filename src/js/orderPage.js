8c6a56406b32b863b6e333b206c4f7b7203de9df
john21429 <john21429@gmail.com>
2022-07-01 15:22:02 +0800

    update

--- src/js/orderPage.js
+++ src/js/orderPage.js
@@ -1,20 +1,145 @@
 
-
 $(function () {	
+    // Vue.component('v-date-picker',{
+    //     data() {
+    //         return {
+    //             range: {
+    //                 start: new Date(2020, 0, 1),
+    //                 end: new Date(2020, 0, 5)
+    //               },
+    //         }
+    //     },
+    //     template:`<v-date-picker v-model="range" is-range/>`
+    // })
 
     let vm = new Vue({
         el:'#app',
-        data:{
-            count:0,
-        },
+        data() {
+            return {
+              time: {
+                year: new Date().getFullYear(),
+                month: new Date().getMonth(),
+              },
+              calendarArr: [],
+              count:0,
+            };
+          },
         methods: {
             min(){
                 if(this.count >= 2){
                     this.count-=1
                 }
             },
+            test(e) {
+                console.log(e);
+                console.log(123);
+                e.classList.add("active");
+              },
+              reduceMonth() {
+                if (this.time.month > 0) {
+                  this.time.month = this.time.month - 1;
+                } else {
+                  this.time.month = 11;
+                  this.time.year = this.time.year - 1;
+                }
+              },
+              addMonth() {
+                if (this.time.month < 11) {
+                  this.time.month = this.time.month + 1;
+                } else {
+                  this.time.month = 0;
+                  this.time.year = this.time.year + 1;
+                }
+              },
+              getDateValue(day,month){
+                // console.log(day,(month+1));
+              },
+              startDay(e){
+                console.log($(e.target));
+                // $(e.target).parent().parent().find('.startDay').removeClass('startDay')
+                // e.target.parentNode.classList.add("startDay");
+                if(e.target.parentNode.childNodes.classList.contains('.startDay')){
+                    // $(e.target).parent().parent().find('.startDay').removeClass('startDay')
+                    console.log("aaa");
+                    e.target.parentNode.classList.add("EndDay");
+                }else{
+                    console.log("BBB");
+                    e.target.parentNode.classList.add("startDay");
+                }
+                
+                // $(e.target).parent().parent().find('.startDay').removeClass('EndDay')
+
+                
+              },
+            //   deleteall(e){
+            //     $(e.target).parent().parent().find('.startDay').removeClass('startDay')
+            //     e.target.parentNode.classList.add("startDay");
+                
+            //   },
+              RangeHover(e){
+                // console.log(e.target);
+                if($(e.target).parent().parent().find('.startDay')){
+                //    console.log($('.startDay'));
+                }
+              }
 
         },
+        computed: {
+            // 獲取當前月份顯示日曆,共42天
+            visibleCalendar: function () {
+              // 獲取今天的年月日
+              const today = new Date();
+              today.setHours(0);
+              today.setMinutes(0);
+              today.setSeconds(0);
+              today.setMilliseconds(0);
+              const calendarArr = [];
+              // 獲取當前月份第一天
+              const currentFirstDay = new Date(
+                this.time.year,
+                this.time.month,
+                1
+              );
+              // 獲取第一天是周幾
+              const weekDay = currentFirstDay.getDay();
+              // 用當前月份第一天减去周幾前面幾天，就是看見的日曆的第一天
+              const startDay = currentFirstDay - (weekDay - 1) * 24 * 3600 * 1000;
+              // 我們統一用42天來顯示當前顯示日曆
+              for (let i = 0; i < 35; i++) {
+                const date = new Date(startDay + i * 24 * 3600 * 1000);
+                calendarArr.push({
+                  date: new Date(startDay + i * 24 * 3600 * 1000),
+                  year: date.getFullYear(),
+                  month: date.getMonth(),
+                  day: date.getDate(),
+                  // 是否在當月
+                  thisMonth:
+                    date.getFullYear() === today.getFullYear() &&
+                    date.getMonth() === today.getMonth()
+                      ? "thisMonth"
+                      : "",
+                  // 是否是今天
+                  isToday:
+                    date.getFullYear() === today.getFullYear() &&
+                    date.getMonth() === today.getMonth() &&
+                    date.getDate() === today.getDate()
+                      ? "isToday"
+                      : "",
+                  // 是否在今天之後
+                  afterToday:
+                    date.getTime() >= today.getTime() ? "afterToday" : "",
+                beforeToday:
+                    date.getTime() <= today.getTime() ? "beforeToday" : "",
+                });
+              }
+              return calendarArr;
+            },
+          },
+          updated() {
+
+
+          },
+
         
     })
     //傳送人數
@@ -124,7 +249,9 @@ $(function () {
     // $('#dateBookin').on('apply.daterangepicker',function(){
     //     console.log($('#dateBooking').val());
     // })
-
+    // $(window).load(()=>{
+    //     $('#dateBooking').on('show.daterangepicker')
+    // })
     //日曆操作區塊
     $('#dateBooking').daterangepicker({
         "timePicker24Hour": true,
@@ -134,6 +261,9 @@ $(function () {
         "opens": "center",
         "drops": "auto",
         "minDate":new Date(),
+        locale:{showLabel: 'true'}
+        
+
     });
 
     $('#dateBooking').on('apply.daterangepicker', function(ev, picker) {
