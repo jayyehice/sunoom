512e43d4778166a6c55fe65263036a48cce5de60
Evonne-Chang <lovelikeangela@hotmail.com>
2022-07-06 01:57:00 +0800

    index butoon test

--- src/js/index.js
+++ src/js/index.js
@@ -90,4 +90,25 @@ function updatePercentage() {
 // gsap.to('.draw01', {duration: 1, width: '65px'});
 
 
+//button animation
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },1000);
+};
+
+var bubblyButtons = document.getElementsByClassName("viewButton");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+  setTimeout("location.href='./food.html'",1000);
+}
+
+
 
